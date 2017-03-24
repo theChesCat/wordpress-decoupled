d02a3771506a33af85b74a92bd348610917dd065
Paloma <paloma.sedeau@gmail.com>
2017-03-24 17:10:58 +0100

    initial commit :
    React base with Webpack, Babel, and ESLint

---
+++ app/index.js
@@ -0,0 +1,9 @@
+/* global document */
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Home from './components/home'
+import './global.css'
+
+ReactDOM.render(<Home />, document.getElementById('app'))
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: './app/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})
+
+module.exports = {
+    entry: './app/index.js',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'index.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loaders: ['babel-loader', 'eslint-loader']
+            },
+            {
+                test: /\.css$/,
+                loaders: [
+                    'style-loader?sourceMap',
+                    'css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        HtmlWebpackPluginConfig
+    ]
+}
